cfc3eb8e4e5ab3dad95ad9fdd03c274ffdc145d8
jupiter <jupiter@gccompany.co.kr>
2024-10-17 17:01:01 +0900

    feat: kind 예제 개선

--- kind/2-load.sh
+++ kind/2-load.sh
@@ -5,3 +5,7 @@ kind load docker-image game-server:1.0-slim
 kind load docker-image game-server:1.0-tiny
 
 kind load docker-image game-server:1.0-tiny-special
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
+kubectl wait -n ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=180s
+kubectl apply -f ingress.yaml
